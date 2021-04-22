c62862eb82dcab753933aa06b27eb2de8a41c7bb
pmcgoohan <6204752+pmcgoohan@users.noreply.github.com>
2021-04-22 18:18:47 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace AlexLatencyWindow
     /// Written to support the thesis in 'Targeting Zero MEV - A Content Layer Solution' that randomizing transaction order within the latency width of a trustless network is as fair as any single honest node's view of the transaction order.
     ///
     /// For an explanation, consider this idealized view of the network:
-    /// - Nodes are distributed around the world and latency is linear to the geographic distance between two nodes.
+    /// - Nodes are equally distributed around the world and latency is linear to the geographic distance between two nodes.
     /// - Each node sends one transaction to the pool over a 10 second period.
     /// - In this model we know precisely when each transaction was sent (in the real world this is impossible to know with zero trust).
     /// - For each node and each transaction we compare the node's view of the transaction timestamp (arrival time) with the objective view (send time).
